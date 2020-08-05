2a257859aa21a1adbfadbc90f6cd30da7fd9ed8b
Heather Barr <hnbarr@yahoo.com>
2020-08-04 19:30:09 -0500

    adding new todo component, importing into app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './components/todo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Todo/>
       </header>
     </div>
   );
---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <p>test this is my todo</p>
+            </div>
+        )
+    }
+}
